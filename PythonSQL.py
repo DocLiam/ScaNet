c0f464794a3a8f0412fbd0a5f3624915ce4e0c7d
Liam <51264013+DocLiam@users.noreply.github.com>
2020-10-24 01:12:41 +0200

    Update PythonSQL.py

--- PythonSQL.py
+++ PythonSQL.py
@@ -1,10 +1,5 @@
-import pyodbc
-
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\progr\Documents\sandbox\Python\test.accdb;')
-cursor = conn.cursor()
-
 def join_multiple(arrayList, delimList):
-    """Joins multiple arrays with specific relevant delimeters."""
+    """Joins multiple arrays with specific relevant delimeters"""
     stringReturn = ""
     listLen = len(arrayList[0])
 
@@ -16,20 +11,33 @@ def join_multiple(arrayList, delimList):
 
     return stringReturn[:-2]
 
+def where_table(fieldArray1, fieldArray2, selectName):
+    """Returns a query selection statement using two compared fields and the operator for comparison"""
+    queryComponent = "WHERE " + join_multiple([fieldArray1, selectName, fieldArray2], [" ", " ", ", "])
+
+    return queryComponent
+
+def update_table(tblName, attributeArray, valuesArray):
+    """Returns a query component that will update the values of attributes in a table"""
+    queryComponent = "UPDATE " + tblName + "\nSET " + join_multiple([attributeArray, valuesArray], [" = ", ", "])
+
+    return queryComponent
+
 def order_table(attributeArray, orderArray):
     """Returns query component that will sort by attributes according to a specified order"""
-    queryComponent = "ORDER BY " + join_multiple([attributeArray, orderArray], [" ", ","])
+    queryComponent = "ORDER BY " + join_multiple([attributeArray, orderArray], [" ", ", "])
 
     return queryComponent
 
 def create_table(tblName, attributeArray, typeArray):
     """Returns a query component that will create a table with attributes and given types"""
-    queryComponent = "CREATE TABLE " + tblName + " (\n" + join_multiple([attributeArray, typeArray], [" ", "\n"]) + "\n);"
+    queryComponent = "CREATE TABLE " + tblName + " (\n" + join_multiple([attributeArray, typeArray], [" ", "\n"]) + "\n)"
 
     return queryComponent
 
 def alter_table(tblName, attributeName, typeName):
-    queryComponent = "ALTER TABLE " + tblName + "\nADD " + attributeName + " " + typeName + ";"
+    """Returns a query that will add a new attribute to a table"""
+    queryComponent = "ALTER TABLE " + tblName + "\nADD " + attributeName + " " + typeName
 
     return queryComponent
 
