9d269c7b8ed00ebe7f7b6977a8c6ef6b9daf014c
Liam <51264013+DocLiam@users.noreply.github.com>
2020-10-23 19:20:51 +0200

    Update PythonSQL.py

--- PythonSQL.py
+++ PythonSQL.py
@@ -7,19 +7,38 @@ cursor.execute("""
                 where * = '1'
                 """)
 
-def join_multiple(arrayList):
+#pls dont edit danre
+def join_multiple(arrayList, delimList):
+    """Joins multiple arrays with specific relevant delimeters."""
     stringReturn = ""
+    listLen = len(arrayList[0])
 
-def order_table(paramArray, orderArray):
-    queryComponent = "ORDER BY " +
+    for i in range(listLen):
+        for j in range(len(arrayList)):
+            stringReturn += arrayList[j][i] + delimList[j]
 
+        stringReturn += " "
 
-def create_table(tblName, )
+    return stringReturn[:-2]
+
+#also no edit
+def order_table(attributeArray, orderArray):
+    """Returns query component that will sort by attributes according to a specified order"""
+    queryComponent = "ORDER BY " + join_multiple([attributeArray, orderArray], [" ", ","])
+
+    return queryComponent
+
+#dont even think about it bro
+def create_table(tblName, attributeArray, typeArray):
+    """Returns a query component that will create a table with attributes and given types"""
+    queryComponent = "CREATE TABLE " + tblName + " (\n" + join_multiple([attributeArray, typeArray], [" ", "\n"]) + "\n);"
+
+    return queryComponent
 
 def search_table(tblName, valuesArray = ["*"], filterArray = [""], orderArray = ["Asc"]):
     for i in range(len(valuesArray) - len(filterArray)):
         filterArray.append("")
     string = "Select "+",".join(valuesArray)+" from "+tblName
-    string +=" where "+", ".join([valuesArray[i] + " = " + filterArray[i] for i in range(len(valuesArray))])
-    string += "Order By " + ",".join(orderArray)
+    string += " where "+", ".join([valuesArray[i] + " = " + filterArray[i] for i in range(len(valuesArray))]) #Upon creation, join_multiple() did not exist. Line is a single line alternative.
+    string += " Order By " + ",".join(orderArray)
     print(string)
