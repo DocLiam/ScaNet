25ceac30d73cc6cfd420bb5cbbd119a2cfd2855e
Liam <51264013+DocLiam@users.noreply.github.com>
2020-10-24 02:15:23 +0200

    Update ScaNetBot.js

--- ScaNetBot.js
+++ ScaNetBot.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!";
+var content;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+  content = message.content
+  channel = message.channel
+
+  if((content.startsWith("!") && channel.type != "dm") || (!content.startsWith("!") && channel.type == "dm")){
+    if (content.endsWith("help")) {
+
+    }
+    if (content.endsWith("register")) {
+
+    }
   }
 });
 
-client.login('TOKEN');
+client.login('NzY5MzQ0NTE1NDA3NzQwOTU5.X5Np6g.K7KAlBNFRLwbXPoHL1Hs8t2Q--U');
