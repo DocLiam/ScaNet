5be8b93dc986ef96bfe337a4bb383c324d9bf5fb
DocLiam <lwats21@somcol.co.za>
2020-10-27 16:12:21 +0200

    Update PythonSQL.py

--- PythonSQL.py
+++ PythonSQL.py
@@ -1,47 +1,54 @@
-def join_multiple(arrayList, delimList):
+def join_multiple(arrayList, delimList, skipLast):
     """Joins multiple arrays with specific relevant delimeters"""
     stringReturn = ""
     listLen = len(arrayList[0])
+    cutLength = 0
 
     for i in range(listLen):
         for j in range(len(arrayList)):
             stringReturn += arrayList[j][i] + delimList[j]
 
-        stringReturn += " "
+    if skipLast:
+        cutLength = len(stringReturn.split(delimList[-1])[-2])
 
-    return stringReturn[:-2]
+    return stringReturn[:-2-cutLength]
 
-def where_table(fieldArray1, fieldArray2, comparatorArray, operatorArray):
+def where_table(fieldArray1, fieldArray2, comparatorArray, operatorArray, skipLast):
     """Returns a query selection statement using two compared fields and the operator for comparison"""
-    queryComponent = "WHERE " + join_multiple([fieldArray1, comparatorArray, fieldArray2, operatorArray], [" ", " ", " ", " "])
+    queryComponent = "WHERE " + join_multiple([fieldArray1, comparatorArray, fieldArray2, operatorArray], [" ", " ", " ", " "], skipLast)
 
     return queryComponent
 
-def update_table(tblName, attributeArray, valuesArray):
+def update_table(tblName, attributeArray, valuesArray, skipLast):
     """Returns a query component that will update the values of attributes in a table"""
-    queryComponent = "UPDATE " + tblName + "\nSET " + join_multiple([attributeArray, valuesArray], [" = ", ", "])
+    queryComponent = "UPDATE " + tblName + "\nSET " + join_multiple([attributeArray, valuesArray], [" = ", ", "], skipLast)
 
     return queryComponent
 
-def order_table(attributeArray, orderArray):
+def order_table(attributeArray, orderArray, skipLast):
     """Returns query component that will sort by attributes according to a specified order"""
-    queryComponent = "ORDER BY " + join_multiple([attributeArray, orderArray], [" ", ", "])
+    queryComponent = "ORDER BY " + join_multiple([attributeArray, orderArray], [" ", ", "], skipLast)
 
     return queryComponent
 
-def create_table(tblName, attributeArray, typeArray):
+def create_table(tblName, attributeArray, typeArray, skipLast):
     """Returns a query component that will create a table with attributes and given types"""
-    queryComponent = "CREATE TABLE " + tblName + " (\n" + join_multiple([attributeArray, typeArray], [" ", "\n"]) + "\n)"
+    queryComponent = "CREATE TABLE " + tblName + " (\n" + join_multiple([attributeArray, typeArray], [" ", ",\n"], skipLast) + "\n)"
 
     return queryComponent
 
-def alter_table(tblName, attributeName, typeName):
+def alter_table(tblName, attributeName, typeName, skipLast):
     """Returns a query that will add a new attribute to a table"""
     queryComponent = "ALTER TABLE " + tblName + "\nADD " + attributeName + " " + typeName
 
     return queryComponent
 
-print(where_table())
+#TESTING
+print(where_table(["ID", "Surname"], ['"123"', '"Smith"'], ["==", "=="], ["AND", "AND"], True))
+print(update_table("DetailsTbl", ["ID", "Surname"], ['"123"', '"Smith"'], False))
+print(order_table(["ID", "Surname"], ["DESC", "ASC"], False))
+print(create_table("DetailsTbl", ["ID", "Surname"], ["int", "varchar(32)"], False))
+print(alter_table("DetailsTbl", "Firstname", "varchar(32)", False))
 
 def search_table(tblName, valuesArray = ["*"], filterArray = [""], orderArray = ["Asc"]):
     for i in range(len(valuesArray) - len(filterArray)):
