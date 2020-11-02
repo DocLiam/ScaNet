e89c2046806f019c441a3ee28e81b1f3edd4d1fc
DocLiam <lwats21@somcol.co.za>
2020-11-03 01:01:27 +0200

    Update ScaNetBot.py

--- ScaNetBot.py
+++ ScaNetBot.py
@@ -1,7 +1,18 @@
 #Library imports
 import Discord
 from discord.ext import commands
+import pyodbc
 from PythonSQL import *
+from datetime import *
+
+#Database setup
+DatabaseConn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\progr\Documents\GitHub\ScaNet\ScaNet.accdb;')
+QueryCursor = DatabaseConn.cursor()
+
+#Fetching last UserID
+QueryString = "SELECT TOP 1 UserID FROM BasicUserData\n" + order_table(["UserID"], ["DESC"], False)
+QueryResult = QueryCursor.execute(QueryString)
+MaxUserID = QueryResult.fetchall()
 
 #Bot setup
 bot = commands.Bot(command_prefix = "!")
@@ -13,7 +24,12 @@ async def on_ready():
 
 #Help command
 @bot.command()
-
+async def register(ctx, *args):
+    NewUserID = MaxUserID+1
+    NewUsername = args[0]
+    NewUserPassword = args[1]
+    DiscordID = ctx.user.id
+    NewRegisterDate = datetime.now()
 
 
 #Bot startup
