f679d221d91662dd0d7595536df5ce5b65c5b969
DocLiam <lwats21@somcol.co.za>
2020-11-06 19:32:57 +0200

    commit to master

--- ScaNetBot.js
+++ ScaNetBot.js
@@ -1,48 +1,71 @@
 //Bot setup
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const prefix = "!";
 
-var content;
-var channel;
-var author;
+var BotHostGuild;
+var BotHostChannel;
+
+var MessageContent;
+var MessageChannel;
+var MessageAuthor;
+
+var ContentArray;
+
 var HelpEmbed;
+var PostEmbed;
+
+var GitHubURL;
 
 //Confirmation of connection established
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on('ready', () => {
+  BotHostGuild = bot.guilds.cache.get("769355986619596841");
+  BotHostChannel = bot.channels.cache.get("769356229310939137");
+  BotHostChannel = bot.channels.cache.get("773198263380869139");
+  
+  console.log(`Logged in as ${bot.user.tag}`);
 });
 
 //Message received
-client.on('message', message => {
-  content = message.content
-  channel = message.channel
-  author = message.author
+bot.on('message', message => {
+  MessageContent = message.content;
+  MessageChannel = message.channel;
+  MessageAuthor = message.author;
+  ContentArray = MessageContent.split(" ");
 
-  if(content.startsWith(prefix)){
+  if(MessageContent.startsWith(prefix)){
     //Help command
-    if (content.startsWith(prefix + "help")) {
+    if (ContentArray[0].endsWith("help")) {
       HelpEmbed = new Discord.MessageEmbed()
       .setColor("#a200ff")
       .setTitle("**Help**")
-      .setAuthor(author.username, author.avatarURL())
+      .setAuthor(MessageAuthor.username, MessageAuthor.avatarURL())
       .addField("!register (User Name) (User Password)", "Register your Discord account as a ScaNet account, with a Username and Password.", false)
       .addField("!post (GitHub Link)", "Post your repository to ScaNet for evaluation or submission using a GitHub Repository URL. Be sure to have a description in the Repository.", false);
 
-      channel.send(HelpEmbed);
+      MessageChannel.send(HelpEmbed);
     }
 
     //Register command
-    if (content.startsWith(prefix + "register")) {
+    if (ContentArray[0].endsWith("register")) {
 
     }
 
     //Post command
-    if (content.startsWith(prefix + "post")) {
+    if (ContentArray[0].endsWith("post")) {
+      GitHubURL = ContentArray[1];
 
+      PostEmbed = new Discord.MessageEmbed()
+      .setColor("#7f0000")
+      .setTitle("**Newly Posted Repository**")
+      .setAuthor(MessageAuthor.username, MessageAuthor.avatarURL())
+      .addField("Author ID:", MessageAuthor.id, false)
+      .addField("Repository GitHub URL:", GitHubURL, false);
+
+      BotHostChannel.send(PostEmbed);
     }
   }
 });
 
 //Bot startup
-client.login('NzY5MzQ0NTE1NDA3NzQwOTU5.X5Np6g.K7KAlBNFRLwbXPoHL1Hs8t2Q--U');
+bot.login('NzY5MzQ0NTE1NDA3NzQwOTU5.X5Np6g.K7KAlBNFRLwbXPoHL1Hs8t2Q--U');
